257ca0a9ac989b27562d54d5721c083b48df4f89
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-01-14 16:26:16 -0600

    Transferencia de tareas entre columnas

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop'; 
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 import { ToDo } from '../../models/todo.model';
@@ -33,14 +33,33 @@ export class BoardComponent {
     {
       id: '2',
       title: 'Buy a unicorn'
-    },
+    }
+  ];
+
+  doing: ToDo[] = [
     {
-      id: '2',
+      id: '3',
       title: 'Watch Angular Path in Platzi'
     }
   ];
 
+  done: ToDo[] = [
+    {
+      id: '4',
+      title: 'Play video games'
+    }
+  ];
+
   drop(event: CdkDragDrop<any[]>) {
-    moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
 }
