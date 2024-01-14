4ca13d01565905f05fd5293003e793e7f9d5a430
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-01-14 13:33:26 -0600

    Agregar acordeones

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faClock, faBox, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGear } from '@fortawesome/free-solid-svg-icons';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 
 @Component({
     selector: 'app-boards',
     standalone: true,
     templateUrl: './boards.component.html',
-    imports: [NavbarComponent, FontAwesomeModule]
+    imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule, NgClass]
 })
+
 export class BoardsComponent {
-    faClock = faClock;
     faTrello = faTrello;
     faBox = faBox;
     faWaveSquare = faWaveSquare;
+    faClock = faClock;
+    faAngleUp = faAngleUp;
+    faAngleDown = faAngleDown;
+    faHeart = faHeart;
+    faBorderAll = faBorderAll;
+    faUsers = faUsers;
+    faGear = faGear;
 }
