ad454ee7a60c8926531e00c6c0c1c850f2c5bc23
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-01-21 16:55:26 -0600

    Implementacion password recovery

--- src/app/modules/auth/components/forgot-password-form/forgot-password-form.component.ts
+++ src/app/modules/auth/components/forgot-password-form/forgot-password-form.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ButtonComponent } from "../../../shared/components/button/button.component";
+import { RequestStatus } from '@models/request-status.model';
+import { AuthService } from '@services/auth.service';
 
 @Component({
     selector: 'app-forgot-password-form',
@@ -11,21 +13,32 @@ import { ButtonComponent } from "../../../shared/components/button/button.compon
     imports: [ReactiveFormsModule, FontAwesomeModule, ButtonComponent]
 })
 export class ForgotPasswordFormComponent {
+
   form = this.formBuilder.nonNullable.group({
     email: ['', [Validators.email, Validators.required]],
   });
-  status: string = 'init';
+  status: RequestStatus = 'init';
   emailSent = false;
 
   constructor(
     private formBuilder: FormBuilder,
+    private authService: AuthService
   ) { }
 
   sendLink() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email } = this.form.getRawValue();
-      // TODO: Connect
+      this.authService.recovery(email)
+        .subscribe({
+          next: () => {
+            this.status = 'success';
+            this.emailSent = true;
+          },
+          error: () => {
+            this.status = 'failed'
+          }
+        });
     } else {
       this.form.markAllAsTouched();
     }
--- src/app/modules/auth/components/recovery-form/recovery-form.component.ts
+++ src/app/modules/auth/components/recovery-form/recovery-form.component.ts
@@ -5,6 +5,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { CustomValidators } from '../../../../utils/validators';
+import { RequestStatus } from '@models/request-status.model';
+import { AuthService } from '@services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-recovery-form',
@@ -12,6 +15,7 @@ import { CustomValidators } from '../../../../utils/validators';
     templateUrl: './recovery-form.component.html',
     imports: [ButtonComponent, ReactiveFormsModule, FontAwesomeModule]
 })
+
 export class RecoveryFormComponent {
   form = this.formBuilder.nonNullable.group(
     {
@@ -24,16 +28,42 @@ export class RecoveryFormComponent {
       ],
     }
   );
-  status: string = 'init';
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  token = '';
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.queryParamMap.subscribe(params => {
+      const token = params.get('token');
+      if(token){
+        this.token = token;
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 
   recovery() {
     if (this.form.valid) {
-      // Todo
+      const { newPassword } = this.form.getRawValue();
+      this.status = 'loading';
+      this.authService.changePassword(this.token, newPassword)
+        .subscribe({
+          next: () => {
+            this.status = 'success';
+            this.router.navigate(['/login'])
+          },
+          error: () => {
+            this.status = 'failed';
+          }
+        })
     } else {
       this.form.markAllAsTouched();
     }
