5f982d2f399a0200f6ed50fc790fead5b9100e81
Jose Flores <66032729+Josefloresh94@users.noreply.github.com>
2024-01-21 13:10:18 -0600

    Modulo de registro funcional

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -6,6 +6,8 @@ import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { CustomValidators } from '../../../../utils/validators';
+import { RequestStatus } from '@models/request-status.model';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-register-form',
@@ -17,26 +19,40 @@ export class RegisterFormComponent {
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(6), Validators.required]],
-    confirmPassword: ['', [Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]],
+    confirmPassword: ['', [Validators.minLength(8), Validators.required]],
   }, {
     validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
   });
-  status: string = 'init';
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  showConfirmPassword = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   register() {
     if (this.form.valid) {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
-      console.log(name, email, password);
+      this.authService.register(name, email, password)
+        .subscribe({
+          next: () => {
+            this.status = 'success';
+            this.router.navigate(['/login']);
+          },
+          error: (error) => {
+            // if(error.error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+            //   this.message = 'This user already exists. Do you want to Login?';
+            // }
+            this.status = 'failed';
+          }
+        })
     } else {
       this.form.markAllAsTouched();
     }
